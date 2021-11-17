25edf85d107eac5c7862f21b8b532fb69e6f807a
ujitayuki0329 <ujita.yuki0329@gmail.com>
2021-11-18 01:22:35 +0900

    header実装

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,43 @@
 <template>
   <div>
     <cliant-only>
-      <i class="fas fa-address-card"></i>
+      <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/"><img src="~/assets/img/header.logo.png" alt="ヘッダーロゴ" height="80"></a>
+          <button class="navbar-toggler menu-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+      
+          <div class="collapse navbar-collapse sp-menu" id="navbarSupportedContent">
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" aria-label="Search" placeholder="フリーワードを入力">　
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/my_page/dog_setting"><i class="fas fa-paw icon"></i>愛犬を掲載する</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/favorite"><i class="fas fa-heart icon"></i>お気に入り</a>
+              </li>
+            </ul>
+            <div class="user-button">
+            <div class="test">
+              <a href="#" class="btn btn-outline-danger">会員登録</a>
+              <a href="#" class="btn btn-outline-info">ログイン</a>
+              <!-- <a href="#" class="btn btn-outline-danger" @click="open_register_modal">会員登録</a>
+              <a href="#" class="btn btn-outline-info" @click="open_login_modal">ログイン</a> -->
+            </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <nav id="nav-bar">
+        <div class="nav-bar-title">
+          <h1>わんちゃん好きと愛犬をつなぐプラットフォーム！まずは無料の会員登録から</h1>
+        </div>
+      </nav> 
     </cliant-only>
   </div>
 </template>
