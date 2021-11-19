d8e4b6ed3322e2c208ce83b19c126631474120db
ujitayuki0329 <ujita.yuki0329@gmail.com>
2021-11-19 13:23:08 +0900

    listファイル作成

---
+++ pages/list/popular.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <client-only>
+    <Header />
+    <div id="container"> 
+      <div id="page_title">
+        <h1><i class="fas fa-paw icon" style="color:#ff7d6e;"></i>人気わんちゃん一覧</h1>
+      </div>
+      <div class="container">
+        
+        
+      </div>
+    </div>
+      
+    <Footer />
+    <Copylight />
+    </client-only>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/style.css'
+import '@/assets/css/list.css'
+
+export default{}
+</script>
