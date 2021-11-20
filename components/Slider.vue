878bb349664d4b571d8f0a2b41978ed358d7069a
ujitayuki0329 <ujita.yuki0329@gmail.com>
2021-11-21 01:34:35 +0900

    slider実装

---
+++ components/Slider.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <cliant-only>
+      <div class="slider">
+          <div class="slide-track">
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/カンガール_ドッグ.png" height="100" width="150" alt="" />
+              <p class="dog_name">カンガール_ドッグ</p>
+              </a>
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/トイ_プードル.png" height="100" width="150" alt="" />
+              <p class="dog_name">トイ・プードル</p>
+              </a>
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/ミニチュア_ピンシャー.png" height="100" width="150" alt="" />
+              <p class="dog_name">ミニチュア_ピンシャー</p>
+              </a>
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/ラブラドゥードル.png" height="100" width="150" alt="" />
+              <p class="dog_name">ラブラドゥードル</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/ボストンテリア.png" height="100" width="150" alt="" />
+              <p class="dog_name">ボストンテリア</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/キャバリア.png" height="100" width="150" alt="" />
+              <p class="dog_name">キャバリア</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/ラブラドールレトリバー.png" height="100" width="150" alt="" />
+              <p class="dog_name">ラブラドールレトリバー</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/チワワ.png" height="100" width="150" alt="" />
+              <p class="dog_name">チワワ</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/ダックスフンド.png" height="100" width="150" alt="" />
+              <p class="dog_name">ダックスフンド</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/スタンダード・プードル.png" height="100" width="150" alt="" />
+              <p class="dog_name">スタンダード・プードル</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/ゴールデンレトリーバー.png" height="100" width="150" alt="" />
+              <p class="dog_name">ゴールデンレトリーバー</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/マルチーズ.png" height="100" width="150" alt="" />
+              <p class="dog_name">マルチーズ</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/コーギー.png" height="100" width="150" alt="" />
+              <p class="dog_name">コーギー</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/ドーベルマン.png" height="100" width="150" alt="" />
+              <p class="dog_name">ドーベルマン</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/ポメラニアン.png" height="100" width="150" alt="" />
+              <p class="dog_name">ポメラニアン</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/シーズー.png" height="100" width="150" alt="" />
+              <p class="dog_name">シーズー</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/フレンチブルドッグ.png" height="100" width="150" alt="" />
+              <p class="dog_name">フレンチブルドッグ</p>
+              </a>    
+            </div>
+            <div class="slide">
+              <a href="#">
+              <img src="~/assets/img/dog-img/グレート・デン.png" height="100" width="150" alt="" />
+              <p class="dog_name">グレート・デン</p>
+              </a>    
+            </div>
+          </div>
+        </div>
+    </cliant-only>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/slider.css'
+
+export default{}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,24 @@
   <div>
     <cliant-only>
       <Header/>
+      <carousel
+          :per-page="1"
+          :autoplay="true"
+          :loop="true"
+          :pagination-padding="5"
+          :autoplay-timeout="3000"
+        >
+        <slide class="carousel-wrap">
+          <img class="carousel-img" src="~/assets/img/slide1.png" alt="スライド1">
+        </slide>
+        <slide class="carousel-wrap">
+          <img class="carousel-img" src="~/assets/img/slide2.png" alt="スライド2">
+        </slide>
+        <slide class="carousel-wrap">
+          <img class="carousel-img" src="~/assets/img/slide3.png" alt="スライド3">
+        </slide>
+      </carousel>
+      <Slider />
       <Footer/>
       <Copylight/>
     </cliant-only>
@@ -9,5 +27,21 @@
 </template>
 
 <script>
-export default {}
+import Carousel from 'vue-carousel/src/Carousel.vue'
+import Slide from 'vue-carousel/src/Slide.vue'
+export default {
+  components: {
+    Carousel,
+    Slide
+  },
+  data () {
+    return{
+      imageurls: [
+        require('~/assets/img/slide1.png'),
+        require('~/assets/img/slide2.png'),
+        require('~/assets/img/slide3.png'),
+      ],
+    }
+  }
+}
 </script>
