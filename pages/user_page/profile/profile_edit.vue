7e20dd66827ec4ae5cde695b9921182cab2a9ff6
ujitayuki0329 <ujita.yuki0329@gmail.com>
2021-12-02 01:28:22 +0900

    プロフィール編集ページ追加

--- pages/user_page/profile/profile_edit.vue
+++ pages/user_page/profile/profile_edit.vue
@@ -6,9 +6,57 @@
       <div id="page_title">
         <h1><i class="fas fa-paw icon" style="color:#ff7d6e;"></i>プロフィール編集</h1>
       </div>
-      <div class="container">
+      <div class="mypage-wrap">
+        <div class="form-group">
+          <p>ユーザー名</p>
+          <input type="text" id="dog_name-field" name="dog_name" class="form-control border-input" placeholder="ujita yuki" value="">
+        </div>
+        <div class="form-group">
+          <p>メールアドレス</p>
+          <input type="text" id="dog_age-field" name="dog_age" class="form-control border-input" placeholder="test@test.com" value="">
+        </div>
+        <div class="form-group">
+          <p>郵便番号(ハイフンなし半角英数)</p>
+          <input type="text" id="dog_age-field" name="dog_age" class="form-control border-input" placeholder="1234567" value="">
+        </div>
+        <div class="form-group">
+          <p>住所</p>
+          <input type="text" id="dog_age-field" name="dog_age" class="form-control border-input" placeholder="東京都千代田区1-1-1" value="">
+        </div>
+        <div class="form-group">
+          <p>銀行名</p>
+          <input type="text" id="dog_age-field" name="dog_age" class="form-control border-input" placeholder="三井住友銀行" value="">
+        </div>
+        <div class="form-group">
+          <p>支店名</p>
+          <input type="text" id="dog_age-field" name="dog_age" class="form-control border-input" placeholder="東京支店" value="">
+        </div>
+        <div class="form-group">
+          <p>口座番号(7桁)</p>
+          <input type="text" id="dog_age-field" name="dog_age" class="form-control border-input" placeholder="1234567" value="">
+        </div>
+        <div class="form-group">
+          <p>口座種別</p>
+          <input type="text" id="dog_age-field" name="dog_age" class="form-control border-input" placeholder="普通" value="">
+        </div>
+        <div class="form-group">
+          <p>口座名義カナ(全角)</p>
+          <input type="text" id="dog_age-field" name="dog_age" class="form-control border-input" placeholder="ウジタ ユウキ" value="">
+        </div>
+          
+        <br>
         
-        
+        <div class="mt-5">
+          <a href="/welcome">
+          <button type="button" class="btn btn-outline-dark">保存</button>
+          </a>
+          <a href="/welcome">
+          <button type="button" class="btn btn-outline-danger">キャンセル</button>
+          </a>
+        </div>
+        <div class="mt-5">
+          <p>愛犬を掲載するは<a href="/user_page/publishe/">こちら</a>から</p>
+        </div>
       </div>
     </div>
       
@@ -20,6 +68,7 @@
 
 <script>
 import '@/assets/css/style.css'
+import '@/assets/css/user_page.css'
 
 export default{}
 </script>
