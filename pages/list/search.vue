cddb51f82a45c03896b757d7f599873300249e68
ujitayuki0329 <ujita.yuki0329@gmail.com>
2021-12-01 13:04:54 +0900

    検索一覧ページレイアウト

--- pages/list/search.vue
+++ pages/list/search.vue
@@ -7,8 +7,173 @@
         <h1><i class="fas fa-paw icon" style="color:#ff7d6e;"></i>検索結果一覧</h1>
       </div>
       <div class="container">
-        
-        
+        <div class="card-group">
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="/dog-detail">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          
+          
+        </div>
+        <div class="card-group">
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+        </div>
+        <div class="card-group">
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+          <div class="card">
+            <img src="~/assets/img/dog-img/ボストンテリア.png">
+            <a href="#">
+            <div class="card-body">
+              <h5 class="card-title">〇〇ちゃん　♀</h5>
+              <p class="card-text h6">女の子｜埼玉県</p>
+              <p class="card-text h6">掲載日2021年05月05日</p>
+            </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="nav-links">
+          <a class="prev page-numbers" href="#">«</a><!-- 現在の前のページへのリンク -->
+          <a class="page-numbers" href="#">1</a>
+          <span class="page-numbers current">2</span><!-- 現在閲覧しているページ -->
+          <a class="page-numbers" href="#">3</a>
+          <span class="page-numbers dots">…</span>
+          <a class="page-numbers" href="#">27</a>
+          <a class="next page-numbers" href="#">»</a><!-- 現在の次のページへのリンク -->
       </div>
     </div>
       
