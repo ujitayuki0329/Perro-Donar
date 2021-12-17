12ddb8bc7c7671716c8a85dde9f6a606a8e851cc
ujitayuki0329 <ujita.yuki0329@gmail.com>
2021-12-17 10:08:26 +0900

    チャット画面途中まで作成

--- pages/chat/chat.vue
+++ pages/chat/chat.vue
@@ -7,7 +7,14 @@
         <h1><i class="fas fa-paw icon" style="color:#ff7d6e;"></i>チャットルーム</h1>
       </div>
       <div class="container">
-        
+        <div class="chat-wrap">
+          <div class="chat-content"> 
+            
+          </div>
+          <div class="chat-send-container">
+            <input type="text" class="chat-send-container-inner">
+          </div>
+        </div>
       </div>
     </div>
       
