1f85c59f633e0380311d174a7aba73480d8d065d
ujitayuki0329 <ujita.yuki0329@gmail.com>
2022-02-10 11:35:53 +0900

    マイページ実装

--- pages/user_page/profile/mypage.vue
+++ pages/user_page/profile/mypage.vue
@@ -19,12 +19,44 @@
           </div>
         </div>
         <!-- 右コンテンツ -->
-        <div class="prolile-wrap">
-          <div class="profile-information">
-            
+          <div class="mypage-container">
+            <header>
+              <div class="bio">
+                <img src="http://www.croop.cl/UI/twitter/images/up.jpg" alt="background" class="bg">
+                <div class="desc">
+                  <h3>@carlf</h3>
+                  <p>Carl Fredricksen is the protagonist in Up. He also appeared in Dug's Special Mission as a minor character.</p>
+                </div>
+              </div>
+              <div class="avatarcontainer">
+                <img src="~/assets/img/avater.png" alt="avatar" class="avatar">
+                <div class="hover">
+                    <div class="icon-twitter"></div>
+                </div>
+              </div>
+            </header>
+            <div class="content">
+              <h1>キャンチョメ</h1>
+              <p>一般会員</p>
+              <p>自己紹介文が入ります。自己紹介文が入ります。自己紹介文が入ります。自己紹介文が入ります。</p>
+              <div class="data">
+                <ul>
+                  <li>
+                    5
+                    <span>掲載犬数</span>
+                  </li>
+                  <li>
+                    150
+                    <span>フォロー</span>
+                  </li>
+                  <li>
+                    300
+                    <span>フォロワー</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-         
-        </div>
       </div>
     </div>
     <Footer />
