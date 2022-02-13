2e7bd101619d6f07ef221135f6e7b9efe163c557
ujitayuki0329 <ujita.yuki0329@gmail.com>
2022-02-14 00:53:37 +0900

    プロフィール編集画面実装

--- pages/user_page/profile/profile_edit.vue
+++ pages/user_page/profile/profile_edit.vue
@@ -21,9 +21,53 @@
         <!-- 右コンテンツ -->
         <div class="prolile-wrap">
           <div class="profile-information">
-            
+            <div class="profile_edit_header">
+              <h3>1.プロフィール</h3>
+              <div class="profile_edit_contents">
+                <p>自己紹介文</p>
+                <textarea name="introduce" class="m-form-textarea" placeholder="自己紹介文"></textarea>
+                <p>Twitterアカウント</p>
+                <h5>http://twitter.com/ <input type="text"  name="dog_breed" class="twitter-form" placeholder="twitterアカウント" value=""></h5>
+                
+              </div>
+            </div>
+            <div class="profile_edit_header">
+              <h3>2.プロフィール写真</h3>
+              <div class="profile_edit_contents">
+                <p>ご使用のパソコンに保存された写真の中から掲載したい写真を指定して下さい。jpg/gif/pngファイルのアップが可能です。pdfファイルはアップできませんのでご注意ください。また、10MB以上の画像は登録できません。尚、画像の大きさは、自動的に規定サイズに縮小・リサイズされますので気にせずアップロードして下さい。</p>
+                <div class="profile_upload">
+                  <p>写真</p>
+                  <div class="profile_img">
+                    <img src="~/assets/img/avater.png" alt="avatar">
+                  </div>
+                  <div class="upload_btn">
+                    <label for="file_photo">
+                      ＋写真を選択
+                      <input type="file" id="file_photo" style="display:none;">
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="profile_edit_header">
+              <h3>3.カバー写真</h3>
+              <div class="profile_edit_contents">
+                <p>ご使用のパソコンに保存された写真の中から掲載したい写真を指定して下さい。jpg/gif/pngファイルのアップが可能です。pdfファイルはアップできませんのでご注意ください。また、10MB以上の画像は登録できません。尚、画像の大きさは、自動的に規定サイズに縮小・リサイズされますので気にせずアップロードして下さい。</p>
+                <div class="profile_upload">
+                  <p>写真</p>
+                  <div class="profile_img">
+                    <img src="~/assets/img/avater.png" alt="avatar">
+                  </div>
+                  <div class="upload_btn">
+                    <label for="file_photo">
+                      ＋写真を選択
+                      <input type="file" id="file_photo" style="display:none;">
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-         
         </div>
       </div>
     </div>
@@ -39,3 +83,65 @@ import '@/assets/css/user_page.css'
 
 export default{}
 </script>
+
+<style>
+.profile-information{
+  position: relative;
+}
+.profile_edit_header{
+  background-color: #ddd;
+  padding: 20px;
+  margin-bottom: 20px;
+}
+.profile_edit_contents{
+  padding: 10px;
+}
+.profile_edit_contents p{
+  font-size: 12px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+.profile_edit_contents h5{
+  font-size: 12px;
+}
+.twitter-form{
+  border: 2px solid #ddd;
+}
+.twitter-form:focus{
+  outline: none;
+  border: 5px solid rgb(188, 205, 236);
+}
+
+.profile_upload{
+  width: 50%;
+  height: 250px;
+  background-color: white;
+}
+.profile_upload p{
+  text-align: center;
+  padding: 10px;
+}
+.profile_img{
+  width: 50%;
+  height: 120px;
+  margin: 0 auto;
+  background-color: #ddd;
+}
+.profile_img img{
+  width: 100%;
+  height: 100%;
+}
+.upload_btn{
+  text-align: center;
+  margin-top: 20px;
+}
+label {
+  width: 50%;
+  color: white;  
+  font-size: 10px;
+  background-color: #ff7d6e;
+  padding: 6px;
+}
+
+
+</style>
