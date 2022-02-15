9a210343c2c9c442fa00a3a69fceebacbdcfb538
ujitayuki0329 <ujita.yuki0329@gmail.com>
2022-02-16 00:43:02 +0900

    firebaseデプロイ実行

--- pages/index.vue
+++ pages/index.vue
@@ -37,6 +37,7 @@
 import Carousel from 'vue-carousel/src/Carousel.vue'
 import Slide from 'vue-carousel/src/Slide.vue'
 export default {
+  buildDir: 'nuxt-dist',
   components: {
     Carousel,
     Slide
