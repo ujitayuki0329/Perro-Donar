84acb3f47051de6b3d5bed35e55cee9bd3158a2a
ujitayuki0329 <ujita.yuki0329@gmail.com>
2022-02-23 09:28:01 +0900

    authenticationユーザー登録実装

--- pages/sample/auth.vue
+++ pages/sample/auth.vue
@@ -5,15 +5,25 @@
       <div id="auth">
         <button type="button" @click="signInWithGoogle">Googleでログイン</button>
       </div>
+      <div id="auth">
+        <button type="button" @click="SignUp">ユーザー作成</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { getFirestore, collection, getDocs } from 'firebase/firestore'
-import {getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth"
+import {getAuth, signInWithPopup, GoogleAuthProvider,onAuthStateChanged, createUserWithEmailAndPassword} from "firebase/auth"
 
 export default {
+  data(){
+    return{
+      email:'ujita.rikako1014@gmail.com',
+      password:'test1234',
+    }
+  },
+
   methods: {
     signInWithGoogle(){
       const auth = getAuth()
@@ -21,6 +31,16 @@ export default {
       signInWithPopup(auth, provider).then((result) => {
         this.$router.push({path:'/'})
       }).catch((error) => {
+        console.log(error)
+      })
+    },
+    SignUp(){
+      const auth = getAuth()
+      createUserWithEmailAndPassword(auth,this.email,this.password).then((userCredential) => {
+        console.log(userCredential)
+      })
+      .catch((error) => {
+        alert(error.message)
         console.log(error)
       })
     }
